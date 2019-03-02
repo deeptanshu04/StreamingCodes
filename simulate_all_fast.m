6903c72096a7a87a93a32119d5df15342171baaf
Deeptanshu <deeptanshu04@gmail.com>
2019-03-02 14:53:00 +0530

    Update simulate_all_fast.m

--- simulate_all_fast.m
+++ simulate_all_fast.m
@@ -1,4 +1,4 @@
-function final_output = simulate_all_fast(alpha,beta,sim_length, eps)   %[err_rate,const_a_err,fo_kh_err,mt_err]
+function final_output = simulate_all_fast(alpha,beta,sim_length, eps)   
 
 t=11;b=8; a=4;
 
@@ -13,7 +13,7 @@ load('G_fk_11_8_4')
 %For t=b, Martinian trott is just repetition
 G_c = construction_c;
 
-%Random MDS code
+% MDS code
 m_mds = 4; %Filed size for MDS code
 P = cauchygen(a_mds,a_mds,m_mds);
 I_mat = gf(eye(a_mds),m_mds);
@@ -21,8 +21,8 @@ G_mds = [I_mat P];
 
 error_array = zeros(1,6);
 counter = 0;
-ep = zeros(1,t+k); % needs t values ahead, (k-1) values back.
-ch_state = 0;
+ep = zeros(1,t+k);  % Erasure pattern. Need t values ahead, (k-1) values back.
+ch_state = 0;       
 
 
 while(counter<sim_length)
@@ -35,15 +35,15 @@ while(counter<sim_length)
     
     ch_state = ch_state_out;
     
-    
-    ep = circshift(ep,[0,-1]);
+    ep = circshift(ep,[0,-1]); 
     ep(end) = ep_next;
-    %Decode an erased codeword
     
-    if(ep(k)==1)
+    
+    %Decode an erased codeword
+    if(ep(k)==1)    %if an erasure happens
         mt_success = (ep(k+t)==0); %Due to repetition coding
         
-        if (sw_erasure(ep,t,b,a))
+        if (sw_erasure(ep,t,b,a))  %If the erasure pattern is a valid sliding window one -- can always decode
             a_success =1;
             fo_kh_success =1;
             c_success =1;
